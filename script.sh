db21c8ba1a708f61313686ba43f6864e04004edd
Matheus Santana <matheus11_santana@hotmail.com>
2024-11-09 20:38:52 -0300

    wip

--- script.sh
+++ script.sh
@@ -17,7 +17,8 @@ sudo pacman -S --noconfirm --needed waybar \
 	pavucontrol \
 	git \
 	base-devel \
-	curl
+	curl \
+	lazygit
 }
 
 remove_unnecessary_packages() {
@@ -64,27 +65,41 @@ install_terminal_dependencies() {
 
 
 
-	chsh -s $(which zsh)
+# Install oh-my-zsh.
+0>/dev/null sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+export ZSH_CUSTOM
+# Configure plugins.
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}"/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM}"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTOM}"/plugins/zsh-history-substring-search
+sed -i 's/^plugins=.*/plugins=(git\n extract\n jsontools\n colored-man-pages\n zsh-autosuggestions\n zsh-syntax-highlighting\n zsh-history-substring-search)/g' ~/.zshrc
+# Enable nvm plugin feature to automatically read `.nvmrc` to toggle node version.
+sed -i "1s/^/zstyle ':omz:plugins:nvm' autoload yes\n/" ~/.zshrc
+# Install powerlevel10k and configure it.
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}"/themes/powerlevel10k
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+# Move ".zcompdump-*" file to "$ZSH/cache" directory.
+sed -i -e '/source \$ZSH\/oh-my-zsh.sh/i export ZSH_COMPDUMP=\$ZSH\/cache\/.zcompdump-\$HOST' ~/.zshrc
+# Configure the default ZSH configuration for new users.
+sudo cp ~/.zshrc /etc/skel/
+sudo cp ~/.p10k.zsh /etc/skel/
+sudo cp -r ~/.oh-my-zsh /etc/skel/
+sudo chmod -R 755 /etc/skel/
+sudo chown -R root:root /etc/skel/
+
+
  
-
-	## install oh-my-zsh
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	##install zinit
-	bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-
 }
 
 
 add_dotfiles_in_system() {
 
 	
-	if [ ! -f "$HOME/.zshrc" ]; then
-		ln -s "$PWD/.zshrc" "$HOME"
-	fi
 
 	if [ ! -f "$HOME/.gitconfig" ]; then
 		ln -s "$PWD/.gitconfig" "$HOME"
-	fi
+	fi	
 
 }
 
@@ -94,4 +109,4 @@ add_dotfiles_in_system
 #install_essential_packages
 #install_paru
 #install_other_packages
-#install_terminal_dependencies
+install_terminal_dependencies
