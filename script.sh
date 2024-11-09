784f0d8dec923ac2f2167643e36e251a19a8e837
Matheus Santana <48627043+theguissan@users.noreply.github.com>
2024-11-09 19:10:56 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,86 +1,97 @@
 #!/bin/bash
 
-#/home/m/.local/share/JetBrains/Toolbox/scripts
-#/home/m/.local/share/JetBrains/Toolbox/apps/intellij-idea-ultimate/bin
-##BAIXANDO DEPENDENCIAS##
-#sudo apt install zsh
-#chsh -s $(which zsh)
+install_essential_packages() {
 
-#sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-#bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+sudo pacman -S --noconfirm --needed waybar \
+	neovim \
+	zsh \
+	ttf-jetbrains-mono-nerd \
+	ttf-profont-nerd \
+	noto-fonts-cjk \
+	noto-fonts-emoji \ 
+	noto-fonts \
+	hyprpaper \
+	swaync \
+	nemo \
+	firefox \
+	pavucontrol \
+	git \
+	base-devel \
+	curl
+}
 
-#curl -sS https://starship.rs/install.sh | sh
-sudo apt install unzip \
-	neofetch \
-	rofi \
-	cava \
-	fonts-noto-color-emoji \
-	cava \
-	alacritty \
-	brightnessctl
+remove_unnecessary_packages() {
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/3270.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/CascadiaCode.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
-wget https://github.com/ryanosis/nerd-fonts/releases/download/v3.1.1/ProFont.zip
-#sudo mv 3270.zip CascadiaCode.zip /usr/local/share/fonts
-sudo unzip 3270.zip -d /usr/local/share/fonts
-sudo unzip CascadiaCode.zip -d /usr/local/share/fonts
-sudo unzip FiraCode.zip -d /usr/local/share/fonts
-sudo unzip ProFont.zip -d /usr/local/share/fonts
-
-rm FiraCode.zip CascadiaCode.zip 3270.zip ProFont.zip
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-##GIT##
+	sudo pacman -R --noconfirm dunst
+}
 
 
-rm ~/.gitconfig
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+install_paru() {
 
 
-#ZSH#
 
-sudo rm ~/.zshrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+	if [ ! -f $(which paru) ]; then
+		
+		echo "PARU IS NOT INSTALLED, INSTALLING..."
+	
+		if [ ! -d "paru" ]; then
+			git clone https://aur.archlinux.org/paru.git
+		else
+			"REPO ALREADY CLONED, INSTALLING..."	
+		fi
 
-##ALACRITTY##
+		cd paru && makepkg -si
+		cd .. && rm -rf paru/
 
-rm ~/.config/alacritty/alacritty.yml
-ln -s ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+	fi
 
-##NEOFETCH##
-
-rm ~/.config/neofetch/config.conf
-ln -s ~/dotfiles/config.conf ~/.config/neofetch/config.conf
-
-#STARSTHIP
-
-rm ~/.config/starship.toml
-ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
-
-##i3##
-
-rm ~/.config/i3/config
-ln -s ~/dotfiles/config ~/.config/i3/config
-
-##i3blocks##
-
-rm ~/.i3blocks.conf
-ln -s ~/dotfiles/.i3blocks.conf ~/.i3blocks.conf
+	echo "UPDADING PACKAGES..."
+	paru --noconfirm -Syu
+}
 
 
-##power-menu##
-sudo ln -s ~/dotfiles/rofi-power-menu /usr/bin/rofi-power-menu
+install_other_packages() {
 
-##picom##
+	paru -Syu --noconfirm --needed google-chrome \
+		nwg-look \
+		codium \
+		vlc \
+		colorls
+}
 
-sudo ln ~/dotfiles/picom.conf ~/.config/picom.conf
 
-##dunst##
-sudo ln -s ~/dotfiles/dunstrc ~/.config/dunst/dunstrc
+install_terminal_dependencies() {
 
-##rofi theme##
 
-mkdir -p ~/.local/share/rofi/{themes,}
 
-sudo ln -s ~/dotfiles/squared-everforest.rasi ~/.local/share/rofi/themes/squared-everforest.rasi
+	chsh -s $(which zsh)
+ 
+
+	## install oh-my-zsh
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	##install zinit
+	bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+}
+
+
+add_dotfiles_in_system() {
+
+	
+	if [ ! -f "$HOME/.zshrc" ]; then
+		ln -s "$PWD/.zshrc" "$HOME"
+	fi
+
+	if [ ! -f "$HOME/.gitconfig" ]; then
+		ln -s "$PWD/.gitconfig" "$HOME"
+	fi
+
+}
+
+
+add_dotfiles_in_system
+#remove_unnecessary_packages
+#install_essential_packages
+#install_paru
+#install_other_packages
+#install_terminal_dependencies
